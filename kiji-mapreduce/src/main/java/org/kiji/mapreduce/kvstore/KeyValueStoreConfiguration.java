9f3cf1b0bc3a5bfd4af7a3b33140d8a5ad415abb
Aaron Kimball <aaron@wibidata.com>
2013-01-23 18:03:46 -0800

    Intermediate: Remove NamespaceConfiguration.
    KeyValueStoreConfiguration now just directly includes the
    NamespaceConfiguration logic.

--- kiji-mapreduce/src/main/java/org/kiji/mapreduce/kvstore/KeyValueStoreConfiguration.java
+++ kiji-mapreduce/src/main/java/org/kiji/mapreduce/kvstore/KeyValueStoreConfiguration.java
@@ -24,11 +24,21 @@ import java.util.Map.Entry;
 import org.apache.hadoop.conf.Configuration;
 
 import org.kiji.annotations.ApiAudience;
-import org.kiji.mapreduce.context.NamespaceConfiguration;
 
-/** Used to serialize KeyValueStore information into a unique namespace. */
+/**
+ * Used to serialize KeyValueStore information into a unique namespace
+ * inside a Configuration object. All conf keys written to this object (e.g., "foo")
+ * are prefixed by a namespace ("ns") specific to the particular key-value store
+ * instance (e.g., "ns.foo").
+ */
 @ApiAudience.Framework
-public final class KeyValueStoreConfiguration extends NamespaceConfiguration {
+public final class KeyValueStoreConfiguration {
+
+  /** The parent Configuration to write to. */
+  private final Configuration mDelegate;
+  /** The namespace to write in. */
+  private final String mNamespace;
+
   /**
    * KeyValueStore definitions are serialized to the Configuration as a set of
    * keys under the "kiji.kvstores.[i]" namespace.
@@ -64,6 +74,260 @@ public final class KeyValueStoreConfiguration extends NamespaceConfiguration {
    * @param storeIndex The namespace index to write to.
    */
   public KeyValueStoreConfiguration(Configuration parent, int storeIndex) {
-    super(parent, KEY_VALUE_STORE_NAMESPACE + storeIndex);
+    this(parent, KEY_VALUE_STORE_NAMESPACE + storeIndex);
+  }
+
+  /**
+   * Constructs a new KeyValueStoreConfiguration that will read and write
+   * values to the parent Configuration, under the specified namespace.
+   *
+   * @param parent The Configuration to back this KeyValueStoreConfiguration.
+   * @param namespace The namespace to write to.
+   */
+  public KeyValueStoreConfiguration(Configuration parent, String namespace) {
+    if (null == parent || null == namespace) {
+      throw new IllegalArgumentException("Parent configuration and namespace must be non-null.");
+    }
+    mDelegate = parent;
+    mNamespace = namespace;
+  }
+
+  /**
+   * Returns the parent Configuration that backs this KeyValueStoreConfiguration.
+   *
+   * @return The parent Configuration.
+   */
+  public Configuration getDelegate() {
+    return mDelegate;
+  }
+
+  /**
+   * Returns the namespace that this KeyValueStoreConfiguration works in.
+   *
+   * @return The namespace.
+   */
+  public String getNamespace() {
+    return mNamespace;
+  }
+
+  /**
+   * Get the value of the <code>name</code> property, or null
+   * if no such property exists.
+   *
+   * @param name The property to get a value for.
+   * @return The value, or null, if no such property exists.
+   */
+  public String get(String name) {
+    return mDelegate.get(prefix(name));
+  }
+
+  /**
+   * Get the value of the <code>name</code> property.
+   *
+   * @param name The property to get a value for.
+   * @param defaultValue The default value if none is specified.
+   * @return The value.
+   */
+  public String get(String name, String defaultValue) {
+    return mDelegate.get(prefix(name), defaultValue);
+  }
+
+  /**
+   * Sets the <code>value</code> of the <code>name</code> property.
+   *
+   * @param name The property to set.
+   * @param value The value to set.
+   */
+  public void set(String name, String value) {
+    mDelegate.set(prefix(name), value);
+  }
+
+  /**
+   * Get the value of the <code>name</code> property as a boolean.
+   *
+   * @param name The property to get a boolean for.
+   * @param defaultValue The value to return if it is not set.
+   * @return The value of this property as a boolean.
+   */
+  public boolean getBoolean(String name, boolean defaultValue) {
+    return mDelegate.getBoolean(prefix(name), defaultValue);
+  }
+
+  /**
+   * Sets the value of the <code>name</code> property to a boolean.
+   *
+   * @param name The property to set.
+   * @param value The boolean to set.
+   */
+  public void setBoolean(String name, boolean value) {
+    mDelegate.setBoolean(prefix(name), value);
+  }
+
+  /**
+   * Gets the value of the <code>name</code> property as a Class.
+   *
+   * @param name The property to get a Class for.
+   * @param defaultValue The value to return if it is not set.
+   * @return The value of this property as a Class.
+   */
+  public Class<?> getClass(String name, Class<?> defaultValue) {
+    return mDelegate.getClass(prefix(name), defaultValue);
+  }
+
+  /**
+   * Get the value of the <code>name</code> property as a Class implementing the
+   * interface specified by <code>xface</code>.  If no such property is specified,
+   * then <code>defaultValue</code> is returned.  An exception is thrown if the
+   * returned class does not implement the named interface.
+   *
+   * @param <U> The interface to deserialize this class to.
+   * @param name The property to get a Class for.
+   * @param defaultValue The value to return if it is not set.
+   * @param xface The interface implemented by the named class.
+   * @return The value of this property as a Class implementing <code>xface</code>.
+   */
+  public <U> Class<? extends U> getClass(String name, Class<? extends U> defaultValue,
+      Class<U> xface) {
+    return mDelegate.getClass(prefix(name), defaultValue, xface);
+  }
+
+  /**
+   * Gets the value of the <code>name</code> property as a Class.
+   *
+   * @param name The property to get a Class for.
+   * @return The value of this property as a Class.
+   * @throws ClassNotFoundException If the class is not found.
+   */
+  public Class<?> getClassByName(String name) throws ClassNotFoundException {
+    return mDelegate.getClassByName(prefix(name));
+  }
+
+  /**
+   * Sets the value of the <code>name</code> property to be a Class.
+   *
+   * @param name The property to set.
+   * @param theClass The Class to set.
+   * @param xface The interface implemented by the named class.
+   */
+  public void setClass(String name, Class<?> theClass, Class<?> xface) {
+    mDelegate.setClass(prefix(name), theClass, xface);
+  }
+
+  /**
+   * Gets the value of the <code>name</code> property as a float.
+   *
+   * @param name The property to get a float for.
+   * @param defaultValue The value to return if it is not set.
+   * @return The value of this property as a float.
+   */
+  public float getFloat(String name, float defaultValue) {
+    return mDelegate.getFloat(prefix(name), defaultValue);
+  }
+
+  /**
+   * Sets the value of the <code>name</code> property to be a float.
+   *
+   * @param name The property to set.
+   * @param value The float to set.
+   */
+  public void setFloat(String name, float value) {
+    mDelegate.setFloat(prefix(name), value);
+  }
+
+  /**
+   * Gets the value of the <code>name</code> property as an int.
+   *
+   * @param name The property to get an int for.
+   * @param defaultValue The value to return if it is not set.
+   * @return The value of this property as an int.
+   */
+  public int getInt(String name, int defaultValue) {
+    return mDelegate.getInt(prefix(name), defaultValue);
+  }
+
+  /**
+   * Sets the value of the <code>name</code> property to be an int.
+   *
+   * @param name The property to set.
+   * @param value The int to set.
+   */
+  public void setInt(String name, int value) {
+    mDelegate.setInt(prefix(name), value);
+  }
+
+  /**
+   * Gets the value of the <code>name</code> property as a long.
+   *
+   * @param name The property to get a long for.
+   * @param defaultValue The value to return if it is not set.
+   * @return The value of this property as a long.
+   */
+  public long getLong(String name, long defaultValue) {
+    return mDelegate.getLong(prefix(name), defaultValue);
+  }
+
+  /**
+   * Sets the value of the <code>name</code> property to be a long.
+   *
+   * @param name The property to set.
+   * @param value The long to set.
+   */
+  public void setLong(String name, long value) {
+    mDelegate.setLong(prefix(name), value);
+  }
+
+  /**
+   * Get the comma delimited values of the <code>name</code> property
+   * as an array of <code>String</code>s.
+   *
+   * @param name The property to get a String array for.
+   * @return The value of this property as a String array.
+   */
+  public String[] getStrings(String name) {
+    return mDelegate.getStrings(prefix(name));
+  }
+
+  /**
+   * Get the comma delimited values of the <code>name</code> property
+   * as an array of <code>String</code>s.  If none found, returns
+   * the default value instead.
+   *
+   * @param name The property to get a String array for.
+   * @param defaultValue The value to return if it is not set.
+   * @return The value of this property as a String array.
+   */
+  public String[] getStrings(String name, String... defaultValue) {
+    return mDelegate.getStrings(prefix(name), defaultValue);
+  }
+
+  /**
+   * Set the array of string values for the <code>name</code> property as comma
+   * delimited values.
+   *
+   * @param name The property to set.
+   * @param values The values.
+   */
+  public void setStrings(String name, String... values) {
+    mDelegate.setStrings(prefix(name), values);
+  }
+
+  /**
+   * Get the value of the <code>name</code> property, without doing variable expansion.
+   *
+   * @param name The property to get a raw value for.
+   * @return The raw value.
+   */
+  public String getRaw(String name) {
+    return mDelegate.getRaw(prefix(name));
+  }
+
+  /**
+   * Returns the prefix for all keys stored in this KeyValueStoreConfiguration.
+   *
+   * @param name The String to prefix with the namespace.
+   * @return The prefix for all keys stored in this KeyValueStoreConfiguration.
+   */
+  private String prefix(String name) {
+    return getNamespace() + "." + name;
   }
 }
